4dbc2211415dbb1d57b68a2edca335b7dd7fcae1
Aijie Shu <s524256521@gmail.com>
2021-11-22 01:41:10 +0800

    change(onUpdateUser) 1. add the loop check

--- functions/src/User.ts
+++ functions/src/User.ts
@@ -5,5 +5,8 @@ import * as admin from "firebase-admin";
 export const onUpdateUser = functions.firestore.document("/{users}/{userID}").onUpdate(async (change, context) => {
     const newValue = change.after.data();
     const previousValue = change.before.data();
+    if newValue == previousValue {
+        return 
+    }
     
 })
